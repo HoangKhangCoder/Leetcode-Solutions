406818213dc7725d581d6195d166b14ade7e784d
Khang Nguyen <hoangkhang110615@gmail.com>
2025-11-15 11:55:45 +0000

    Sync LeetCode submission - Sqrt(x) (python3)

---
+++ problems/sqrt(x)/solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        minN, maxN = 0, x
+        while minN <= maxN:
+            mid = (minN + maxN) // 2
+            val = mid ** 2
+            if val <= x and (mid + 1) ** 2 > x:
+                return mid
+            if val < x:
+                minN = mid + 1
+            else:
+                maxN = mid - 1
