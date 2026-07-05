92c79513eae7ca03538c6c4ac3cffeede93251be
Khang Nguyen <hoangkhang110615@gmail.com>
2026-07-05 02:36:23 +0000

    Sync LeetCode submission - Sum of Integers with Maximum Digit Range (python3)

---
+++ problems/sum_of_integers_with_maximum_digit_range/solution.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maxDigitRange(self, nums: list[int]) -> int:
+        range = []
+        _nums = {}
+        
+        for num in nums:
+            digits = list(map(int, list(str(num))))
+            res = max(digits) - min(digits)
+            _nums[num] = res
+            range.append(res)
+            
+        maxRange = max(range)
+        s = 0
+        for num in nums:
+            if _nums[num] == maxRange:
+                s += num
+        return s
+                
