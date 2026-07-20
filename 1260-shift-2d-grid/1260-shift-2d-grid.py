73fb76371162fcc3c52f729ef08e8603ea9346f6
Khang Nguyen <hoangkhang110615@gmail.com>
2026-07-21 06:13:53 +0700

    Time: 819 ms (5.17%), Space: 19.7 MB (19.83%) - LeetHub

---
+++ 1260-shift-2d-grid/1260-shift-2d-grid.py
@@ -0,0 +1,21 @@
+from copy import deepcopy
+class Solution:
+    def shiftGrid(self, grid: List[List[int]], k: int) -> List[List[int]]:
+        m, n = len(grid), len(grid[0])
+        setup = [[0] * n for _ in range(m)]
+        for _ in range(k):
+            # for row in grid:
+                # print(" ".join(list(map(str, row))))
+            # print("=" * 30)
+            new = deepcopy(setup)
+            # print(new)
+            for i in range(m):
+                for j in range(n):
+                        if i == m - 1 and j == n - 1:
+                            new[0][0] = grid[i][j]
+                        elif j == n - 1:
+                            new[i + 1][0] = grid[i][j]
+                        else:
+                            new[i][j + 1] = grid[i][j]
+            grid = deepcopy(new)
+        return grid
