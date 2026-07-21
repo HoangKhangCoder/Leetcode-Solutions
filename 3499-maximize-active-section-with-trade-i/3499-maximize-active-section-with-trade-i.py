0ad53e8c39be5b3b5526ed60679b8f450c0453b9
Khang Nguyen <hoangkhang110615@gmail.com>
2026-07-21 18:24:37 +0700

    Time: 778 ms (42.05%), Space: 21.1 MB (55.14%) - LeetHub

---
+++ 3499-maximize-active-section-with-trade-i/3499-maximize-active-section-with-trade-i.py
@@ -0,0 +1,33 @@
+class Solution:
+    def maxActiveSectionsAfterTrade(self, s: str) -> int:
+        prefix = []
+        oneCon = zeroCon = 0
+        ones = 0
+        conGroups = 0
+        for d in s:
+            if d == "0":
+                zeroCon -= 1
+                if oneCon > 0:
+                    prefix.append(oneCon)
+                    conGroups += 1
+                oneCon = 0
+            else:
+                if zeroCon < 0:
+                    prefix.append(zeroCon)
+                    conGroups += 1
+                oneCon += 1
+                ones += 1
+                zeroCon = 0
+        prefix.append([zeroCon, oneCon][int(s[-1])])
+        conGroups += 1
+        # print(prefix)
+        # print(conGroups)
+        maxWay = ones
+        for i in range(conGroups - 2):
+            if prefix[i] > 0:
+                continue
+            # print(i)
+            actives = abs(prefix[i]) + abs(prefix[i + 2]) + ones
+            # print(abs(prefix[i]), abs(prefix[i + 2]), oneConPrefix[i + 1])
+            maxWay = max(maxWay, actives)
+        return maxWay
