28ff6a2eb35a2c1e96341ed656b6eceb2d4ebd82
Khang Nguyen <hoangkhang110615@gmail.com>
2026-07-29 10:19:54 +0700

    Time: 0 ms (100%), Space: 19.4 MB (33.37%) - LeetHub

--- 0060-permutation-sequence/0060-permutation-sequence.py
+++ 0060-permutation-sequence/0060-permutation-sequence.py
@@ -2,17 +2,17 @@ import math
 
 class Solution:
     def getPermutation(self, n: int, k: int) -> str:
-        nums = [str(i) for i in range(1, n + 1)]
+        nums = [i for i in range(1, n + 1)]
         k -= 1
         
-        result = []
+        res = 0
         fact = math.factorial(n - 1)
         
         for i in range(n - 1, 0, -1):
             idx = k // fact
-            result.append(nums.pop(idx))
+            res = res * 10 + nums.pop(idx)
             k %= fact
             fact //= i
         
-        result.append(nums[0])
-        return "".join(result)
+        res = res * 10 + nums[0]
+        return str(res)
