ae41523066254aff725f2f34ae184875822f09c7
Khang Nguyen <hoangkhang110615@gmail.com>
2026-08-02 08:53:26 +0700

    Time: 0 ms (100%), Space: 10.3 MB (87.75%) - LeetHub

---
+++ 0877-stone-game/0877-stone-game.cpp
@@ -0,0 +1,7 @@
+class Solution {
+public:
+    bool stoneGame(vector<int>& piles) {
+        // Alice always wins due to the even number of piles and odd total stones
+        return true;
+    }
+};
