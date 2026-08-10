6dde8698bdcb9ccec676623aa4ad024551b064a0
Khang Nguyen <hoangkhang110615@gmail.com>
2026-08-10 07:30:39 +0700

    Time: 0 ms (100%), Space: 19.6 MB (98.48%) - LeetHub

--- 1510-stone-game-iv/1510-stone-game-iv.py
+++ 1510-stone-game-iv/1510-stone-game-iv.py
@@ -1,14 +1,15 @@
 from math import isqrt
+maxN = 10 ** 5
+dp = [False] * (maxN + 1)
+squares = [k ** 2 for k in range(1, isqrt(maxN) + 1)]
+for i in range(maxN + 1):
+    if dp[i]:
+        continue
+    for sq in squares:
+        if i + sq > maxN:
+            break
+        dp[i + sq] = True
+        
 class Solution:
     def winnerSquareGame(self, n: int) -> bool:
-
-        dp = [False] * (n + 1)
-        squares = [k ** 2 for k in range(1, isqrt(n) + 1)]
-        for i in range(n + 1):
-            if dp[i]:
-                continue
-            for sq in squares:
-                if i + sq > n:
-                    break
-                dp[i + sq] = True
         return dp[n]
